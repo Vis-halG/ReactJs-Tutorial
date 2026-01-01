94572a58c01cd51c58d58388e67fb6a8536ae554
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2026-01-01 08:51:28 +0530

    update

--- src/components/DynamicValue/DynamicValue.jsx
+++ src/components/DynamicValue/DynamicValue.jsx
@@ -9,6 +9,7 @@ const DynamicValue = () => {
 };
 
 const Card = () => {
+const age = 1;
   const name = "ClarityCam";
   const price = 2499;
   const summary =
@@ -16,6 +17,12 @@ const Card = () => {
   const returnType = () => {
     const type = "Classic";
     return type;
+  };
+  const watch = () =>{
+    if (age == 18) {return "allowed";}
+    
+ 
+    return "not allowed";
   };
 
   return (
@@ -25,6 +32,11 @@ const Card = () => {
         <h3>Price : {price + 20}</h3>
         <h3>Type : {returnType()}</h3>
         <p> Summary : {summary}</p>
+        <button>
+          {age >= 18 ? "watch now" : "not available" }
+        </button>
+
+        <button>{watch()}</button>
       </div>
       <img src="images/camera.png" alt="camera" />
     </>
