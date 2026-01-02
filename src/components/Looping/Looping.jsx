877776b877a02d07bcff43ac540ac01a936a182c
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2026-01-02 14:36:39 +0530

    Map Method

--- src/components/Looping/Looping.jsx
+++ src/components/Looping/Looping.jsx
@@ -1,23 +1,69 @@
+// import seriesData from "../../api/seriesData.json";
+
+// const Looping = () => {
+//   return (
+//     <div>
+//       {seriesData.map((pages) => {
+//         return(
+//         <div key={pages.id}>
+
+//         <div><img src={pages.img_url} alt="" /></div>
+// <h2>Name : {pages.name}</h2>
+//         </div>
+//         )
+//       })}
+//     </div>
+//   )
+// }
+// export default Looping;
+
+// import seriesData from "../../api/seriesData.json";
+
+// const Looping = ( ) => {
+//   return(
+//     <div>
+// {seriesData.map((pages) => {
+//   return(
+//     <div key={pages.id}>
+// <div><img src={pages.img_url} alt="" /></div>
+// <h2>{pages.name}</h2>
+// <h3>Rating : {pages.rating}</h3>
+// <p>Summary : {pages.description} </p>
+// <a href={pages.watch_url}>
+//   <button>Watch Now</button>
+// </a>
+//     </div>
+//   )
+// })}
+//     </div>
+//   )
+// }
+
+// export default Looping;
+
 import seriesData from "../../api/seriesData.json";
 
 const Looping = () => {
   return (
-    <>
-      <div>
-        <div>
-          <img src={seriesData[0].img_url} alt="" width="500pvw" />
-        </div>
-        <h2>Name : {seriesData[0].name}</h2>
-        <h3> Rating : {seriesData[0].rating}</h3>
-        <p>Description : {seriesData[0].description}</p>
-        <p>Cast : {seriesData[0].cast}</p>
-        <p>Genre : {seriesData[0].genre}</p>
-      <a href={seriesData[0].watch_url}>
-        <button>Watch Now</button>
-      </a>
-      </div>
-    </>
+    <div>
+      {seriesData.map((pages) => {
+        return (
+          <div key={pages.id}>
+            <div>
+              <img src={pages.img_url} alt="" />
+            </div>
+            <h2>{pages.name}</h2>
+            <h3>Rating : {pages.rating}</h3>
+            <p>Summary : {pages.description}</p>
+            <p>Cast : {pages.cast}</p>
+            <p>Genre : {pages.genre}</p>
+            <a href={pages.watch_url}>
+              <button>Watch Now</button>
+            </a>
+          </div>
+        );
+      })}
+    </div>
   );
 };
-
 export default Looping;
