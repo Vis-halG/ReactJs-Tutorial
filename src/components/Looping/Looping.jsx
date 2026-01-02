d51e584f88d827d2f74b9408625710e5be22ad8a
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2026-01-02 12:10:33 +0530

    update

--- src/components/Looping/Looping.jsx
+++ src/components/Looping/Looping.jsx
@@ -9,6 +9,12 @@ const Looping = () => {
         </div>
         <h2>Name : {seriesData[0].name}</h2>
         <h3> Rating : {seriesData[0].rating}</h3>
+        <p>Description : {seriesData[0].description}</p>
+        <p>Cast : {seriesData[0].cast}</p>
+        <p>Genre : {seriesData[0].genre}</p>
+      <a href={seriesData[0].watch_url}>
+        <button>Watch Now</button>
+      </a>
       </div>
     </>
   );
